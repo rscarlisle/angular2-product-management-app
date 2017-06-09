b34bda4d247eab23f081604cc82acb0956e3afb1
Randall Carlisle <randy@pd-quick.com>
2017-06-08 23:30:27 -0700

    Update welcome.component.ts

--- app/home/welcome.component.ts
+++ app/home/welcome.component.ts
@@ -1,1 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'app/home/welcome.component.html'
+})
+export class WelcomeComponent {
+    public pageTitle: string = 'Welcome';
+}
 
