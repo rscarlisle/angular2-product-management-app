41861230379023054a47420953da8218481d551b
Randall Carlisle <randy@pd-quick.com>
2017-06-10 12:48:33 -0700

    Create user_administration_spec.js
    The skeleton of a simple unit test for user administrator permission.

---
+++ test/user_administration_spec.js
@@ -0,0 +1,10 @@
+let assert = require("assert");
+let UserAdministration = require("../user_administration.js");
+
+describe("Applying for administrator permissions", function() {
+  describe("Using valid email, first, last", function() {
+    it("is valid");
+    it("reports a valid email");
+    it("reports valid name");
+  });
+});
