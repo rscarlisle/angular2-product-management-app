ee727fb4950f565518572a021fe499e082ca03c4
Randall Carlisle <randy@pd-quick.com>
2017-06-10 11:03:10 -0700

    Create first_spec.js
    Initial test spec using the built-in Node.js Assert library, with nested describe blocks and it block describing behavior.

---
+++ test/first_spec.js
@@ -0,0 +1,8 @@
+let assert = require("assert");
+describe("A feature", function() {
+  describe("A scenario", function() {
+    it("Does something expected", function() {
+      assert(true);
+    });
+  });
+});
