b736856cec12226c59d8007ead203f3df8ce75f2
Randall Carlisle <randy@pd-quick.com>
2017-06-11 20:33:20 -0700

    Create server.js
    Skeleton of a web server.

---
+++ build/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
