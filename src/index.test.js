9c303f0417d71baf6507492edc977e65575ef015
Randall Carlisle <randy@pd-quick.com>
2017-06-13 13:54:44 -0700

    Create index.test.js
    Created my first test in a simple describe / it block.

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
