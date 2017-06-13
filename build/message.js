f27443d9af665996f0f1ea666a9bfec42b5b6f6e
Randy Carlisle <cisolutionsinc@gmail.com>
2017-06-13 10:32:51 -0700

    fix no-control error

--- build/message.js
+++ build/message.js
@@ -1,3 +1,4 @@
 import chalk from "chalk";
 
-console.log(chalk.green("Starting app in developer mode...")); /* eslint-disable-line no-console */
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+console.log(chalk.green("Starting app in developer mode..."));
