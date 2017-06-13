cef2a2c7b5a0590c06dc0462f8a3c05cc2e98609
Randy Carlisle <cisolutionsinc@gmail.com>
2017-06-13 10:22:50 -0700

    fix no-console error

--- build/server.js
+++ build/server.js
@@ -4,8 +4,6 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
-/* eslint-disable-line no-console */
-
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
@@ -20,7 +18,7 @@ app.get('/', function(req, res) {
 
 app.listen(port, function(err) {
     if (err) {
-        console.log(err);
+        console.log(err); /* eslint-disable-line no-console */
     } else {
         open('http://localhost:' + port);
     }
