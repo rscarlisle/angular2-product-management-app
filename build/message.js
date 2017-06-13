5f0609910c4be3acbc192c249f465256022abd70
Randy Carlisle <cisolutionsinc@gmail.com>
2017-06-13 09:59:06 -0700

    lint err - added: eslint-disable no-console

--- build/message.js
+++ build/message.js
@@ -1,2 +1,2 @@
 import chalk from "chalk";
-console.log(chalk.green("Starting app in developer mode..."));
+console.log(chalk.green("Starting app in developer mode...")); /* eslint-disable-line no-console */
