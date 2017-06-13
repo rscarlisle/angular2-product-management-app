c4b214888498206851ab002ce24e585f0babc54b
Randall Carlisle <randy@pd-quick.com>
2017-06-12 17:49:39 -0700

    Create webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,7 +15,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: ['css']}
+      {test: /\.css$/, loader: [style-loader', 'css-loader']}
     ]
   }
 }
