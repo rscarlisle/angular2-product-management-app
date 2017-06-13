a081c10942a3a7dd7028b99c3fac167e42dd9bae
Randall Carlisle <randy@pd-quick.com>
2017-06-13 14:34:12 -0700

    Update index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -12,7 +12,7 @@ describe('index.html', () => {
   it('should say hello', () => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
-      const h1 = window.document.getElementByTagName('h1')[0];
+      const h1 = window.document.getElementsByTagName('h1')[0];
       expect(h1.innerHTML).to.equal("Hello, world!");
       window.close();
     });
