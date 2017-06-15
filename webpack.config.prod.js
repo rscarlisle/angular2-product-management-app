c17eed341d05dd23b293c5e4e48e86fdd11c0de6
Randall Carlisle <randy@pd-quick.com>
2017-06-15 14:14:36 -0700

    Create webpack.config.prod.js
    Production settings and configurations for webpack.

---
+++ webpack.config.prod.js
@@ -0,0 +1,34 @@
+import path from 'path';
+
+export default {
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    // Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+    
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+    module: {
+      loaders: [
+        {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+        {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+      ]
+    }
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.css$/, loaders: ['style-loader', 'css-loader']}
+    ]
+  }
+}
