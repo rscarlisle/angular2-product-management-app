473acb0de65194f479bc354fe37119593dec504e
Randall Carlisle <randy@pd-quick.com>
2017-06-15 16:57:31 -0700

    Create webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,13 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [],
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true,
+    })  
+  ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
