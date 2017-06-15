223804dad61d11e988ec3f7b4b865acb188ad120
Randall Carlisle <randy@pd-quick.com>
2017-06-15 16:06:34 -0700

    Update webpack.config.prod.js
    Commented out DeDupe plugin because of error message.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -16,7 +16,7 @@ export default {
   },
   plugins: [
     // Eliminate duplicate packages when generating bundle
-    new webpack.optimize.DedupePlugin(),
+//     new webpack.optimize.DedupePlugin(),
     
     // Minify JS
     new webpack.optimize.UglifyJsPlugin()
