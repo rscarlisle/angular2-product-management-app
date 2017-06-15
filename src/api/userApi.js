8b691b8fdb19e110f31745bcb8bfee87e906951a
Randall Carlisle <randy@pd-quick.com>
2017-06-15 15:43:43 -0700

    Create userApi.js

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -29,5 +29,5 @@ function onSuccess(response) {
 }
 
 function onError(error) {
-    console.log(error); /* eslint-disable-line no-console */
+    //console.log(error); /* eslint-disable-line no-console */
 }
