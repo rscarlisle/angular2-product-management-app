ff54a7142bd6fc133f08d23a062e0a28c4271e11
Randall Carlisle <randy@pd-quick.com>
2017-06-14 23:09:08 -0700

    Create index.js
    Populate a table of users via API call.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import './index.css';
-import numeral from 'numeral';
-/* eslint-disable no-console */
-const courseValue = numeral(1000).format('$0,0.00');
-// debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`);
+
+import {getUsers, deleteUser} from './api/userApi';
+
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
