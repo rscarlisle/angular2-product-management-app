3faff828b7a65a9e95136d5b4b35ea86a05dc13e
Randall Carlisle <randy@pd-quick.com>
2017-06-15 17:40:51 -0700

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ export default {
   ],
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '/dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
