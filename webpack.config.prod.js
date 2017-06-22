15acee4e6b103e1f88777e2257eb0b92250efbff
Randy Carlisle <cisolutionsinc@gmail.com>
2017-06-22 10:00:34 -0700

    implement cache busting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
 // import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 
@@ -17,20 +18,23 @@ export default {
     filename: '[name].js'
   },
   plugins: [
+    // Hash the files using MD5 so that their names change when the content] changes.
+    new WebpackMd5Hash(),
+
     // Use CommonsChunkPlugin to create a separate bundle
     // of vendor libraries so that they're cached separately.
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
     }),
-    
+
     // Generate an external css file with a hash in the filename
     // new ExtractTextPlugin('[name].[contenthash].css'),
-    
+
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: true,
-      
+
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -42,16 +46,16 @@ export default {
         minifyJS: true,
         minifyCSS: true,
         minifyURLs: true
-      },      
-       
+      },
+
       // Properties you define here are available in index.html
-      // using htmlWebpackPlugin.options.varName      
+      // using htmlWebpackPlugin.options.varName
       trackJSToken: '2a26d136c2824c9a8e7f335611cad4ac'
     }),
-    
+
     // Eliminate duplicate packages when generating bundle
     // new webpack.optimize.DedupePlugin(),
-    
+
     // Minify JS
     new webpack.optimize.UglifyJsPlugin()
   ],
