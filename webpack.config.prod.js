5de8ebfce72545b961fa40f8b879023f8f5e8ee3
Randall Carlisle <randy@pd-quick.com>
2017-06-22 21:49:31 -0700

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,7 +2,7 @@ import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
-// import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
 //   debug: true,
@@ -19,8 +19,8 @@ export default {
     filename: '[name].[chunkhash].js'
   },
   plugins: [
-    // Generate an external css file with a hash in the filename
-    // new ExtractTextPlugin('[name].[contenthash].css'),
+  // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
     
     // Hash the files using MD5 so that their names change when the content] changes.
     new WebpackMd5Hash(),
@@ -63,8 +63,8 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /\.css$/, loaders: ['style', 'css']}
-   // {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}      
+//    {test: /\.css$/, loaders: ['style', 'css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}      
     ]
   }
 };
